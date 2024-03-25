d50f551b487a896540764d55be3edb916402fcec
niabezara <niabezarashvili@yahoo.com>
2024-03-25 15:41:10 +0400

    material ui changed

--- ToDo List/src/components/Create.jsx
+++ ToDo List/src/components/Create.jsx
@@ -1,25 +1,8 @@
 import { useState } from "react";
 import axios from "axios";
-import { TextField, Button, makeStyles, Container } from "@material-ui/core";
-
-const useStyles = makeStyles({
-  input: {
-    width: "70%",
-    marginBottom: 30,
-  },
-  addButton: {
-    height: 55,
-    marginBottom: 30,
-  },
-  container: {
-    textAlign: "center",
-    marginTop: 10,
-  },
-});
 
 export default function Create() {
   const [task, setTask] = useState();
-  const classes = useStyles();
 
   const handleAdd = () => {
     axios
@@ -29,24 +12,15 @@ export default function Create() {
   };
 
   return (
-    <Container className={classes.container}>
-      <TextField
-        variant="outlined"
+    <div>
+      <input
         onChange={(e) => setTask(e.target.value)}
         label="type your task"
         value={task}
-        className={classes.input}
       />
-      <Button
-        size="large"
-        variant={"contained"}
-        color="primary"
-        onClick={handleAdd}
-        className={classes.addButton}
-        disabled={task ? false : true}
-      >
+      <button onClick={handleAdd} disabled={task ? false : true}>
         Add Task
-      </Button>
-    </Container>
+      </button>
+    </div>
   );
 }
--- ToDo List/src/components/Home.jsx
+++ ToDo List/src/components/Home.jsx
@@ -1,52 +1,10 @@
 import { useEffect, useState } from "react";
 import Create from "./Create";
 import axios from "axios";
-import {
-  Container,
-  List,
-  ListItem,
-  Checkbox,
-  Typography,
-  Button,
-  makeStyles,
-} from "@material-ui/core";
-
-const useStyles = makeStyles({
-  input: {
-    width: "70%",
-    marginBottom: 30,
-  },
-  addButton: {
-    height: 55,
-    marginBottom: 30,
-  },
-  container: {
-    textAlign: "center",
-    marginTop: 100,
-  },
-  list: {
-    width: "80%",
-    margin: "auto",
-    display: "flex",
-    justifyContent: "space-around",
-    border: "1px solid light-gray",
-  },
-  text: {
-    width: "70%",
-  },
-  listButtons: {
-    marginLeft: 10,
-  },
-  title: {
-    textAlign: "center",
-    width: "100%",
-    color: "#f50057",
-  },
-});
 
 export default function Home() {
   const [todos, setTodos] = useState([]);
-  const classes = useStyles();
+
   useEffect(() => {
     axios
       .get("http://localhost:3001/")
@@ -81,37 +39,21 @@ export default function Home() {
   };
 
   return (
-    <Container component="main">
-      <Typography variant="h3" className={classes.title}>
-        Todo List
-      </Typography>
-
+    <div>
+      <h3>Todo List</h3>
       <Create />
-
-      <List>
+      <div>
         {todos.map((todo) => (
-          <ListItem divider={true} className={classes.list} key={todo._id}>
-            <Checkbox
+          <div key={todo._id}>
+            <input
               onClick={() => handleUpdate(todo._id, !todo.done)}
               checked={todo.done}
             />
-            <Typography
-              className={classes.text}
-              style={{ color: todo.done ? "green" : "" }}
-            >
-              {todo.task}
-            </Typography>
-            <Button
-              onClick={() => handleDelete(todo._id)}
-              color="secondary"
-              variant="contained"
-              className={classes.listButtons}
-            >
-              Delete
-            </Button>
-          </ListItem>
+            <p style={{ color: todo.done ? "green" : "black" }}>{todo.task}</p>
+            <button onClick={() => handleDelete(todo._id)}>Delete</button>
+          </div>
         ))}
-      </List>
-    </Container>
+      </div>
+    </div>
   );
 }
