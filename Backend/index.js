afda583356d68b880e4e776f8269dc457a241269
niabezara <niabezarashvili@yahoo.com>
2024-03-25 14:42:40 +0400

    get route

--- Backend/index.js
+++ Backend/index.js
@@ -20,6 +20,10 @@ mongoose
 app.use(cors());
 app.use(express.json());
 
+app.get('/', (req, res) => {
+  res.send('Welcome to the Todo List API');
+});
+
 app.get('/get', (req, res) => {
   TodoModel.find()
     .then((result) => {
